d07096b95e27ef3c8a7a9c4d294985dc1e9318cb
kailas matare <105923744+kailasmatare164@users.noreply.github.com>
2023-07-11 17:18:33 +0530

    Create Patturn1.java

---
+++ Patturn1.java
@@ -0,0 +1,33 @@
+Patturn_1
+public class Patturn
+{
+public static void main(String []args)
+{
+
+for (int i = 1; i <=5; i++) 
+       {
+       	 for (int j = 1; j <=5; j++) 
+    	     {
+               System.out.print("*");
+			     }   
+    	       System.out.println();
+	    }
+}
+}
+
+/*  
+Output
+*****
+*****
+*****
+*****
+*****
+*/
+
+
+
+
+
+
+
+
